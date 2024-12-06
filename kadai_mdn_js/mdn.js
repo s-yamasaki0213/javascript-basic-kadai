00fa6e6e0592d6d1a6f0aac082430b776f00c383
s-yamasaki0213 <3651999@gmail.com>
2024-12-07 04:37:47 +0900

    Update mdn.js

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,5 +1,3 @@
 const now_date = new Date;
 
-console.log( now_date.getFullYear() + '年');
-console.log( now_date.getMonth()+1 + '月');
-console.log( now_date.getDay() + '日');
+console.log( now_date.getFullYear() + '年' + (now_date.getMonth()+1) + '月' + now_date.getDay() + '日');
