e2442b3a0f7c89950fd9e2309bbeabc80de675fb
s-yamasaki0213 <3651999@gmail.com>
2024-12-12 02:53:28 +0900

    kadai_021アップロード

---
+++ kadai_021/event.js
@@ -0,0 +1,10 @@
+
+const eventBtn = document.getElementById('btn');
+const text = document.getElementById('text');
+
+eventBtn.addEventListener('click', ()=>{
+    setTimeout( ()=> {
+        text.textContent = 'ボタンをクリックしました'
+    },2000);
+    
+});
